2298e943d1b2add84d5510d3c5424708350c59a1
priscila espinoza <pr.espinoza@duocuc.cl>
2022-10-17 15:42:02 -0300

    bonnus

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-from distutils.log import debug
 from flask import Flask, render_template, session, redirect
 
 app = Flask(__name__)
@@ -10,7 +9,7 @@ def index():
     if 'count' in session: # en esta linea consulto si existe count en session
         session['count'] += 1 # si esxiste utilizamos eel codigo
     else:
-        session['count'] = 0 # sino existe la pdebemos inicialir en 1
+        session['count'] = 0 # sino, la pdebemos inicialir en 1
     
     return render_template('index.html')
 
@@ -18,10 +17,16 @@ def index():
 def destroy_session():
     session.clear()
     return redirect('/')
-    
 
+@app.route('/incremento2')    
+def incremento2():
+    session['count'] +=1 
+    return redirect('/')
 
-
+@app.route('/reiniciar')
+def reiniciar():
+    session.clear()
+    return redirect('/') 
 
 if __name__=="__main__":
     app.run(debug=True)
